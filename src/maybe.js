45923574e0f45c10acd173858ecbe85d17121fa7
pui <5474275+puiutucutu@users.noreply.github.com>
2019-04-11 19:08:06 -0400

    Create maybe.js

---
+++ src/maybe.js
@@ -0,0 +1,32 @@
+// from Javascript Allonge, pg 66
+function maybe(f) {
+  return function() {
+    var i;
+    if (arguments.length === 0) {
+      return;
+    } else {
+      for (i = 0; i < arguments.length; ++i) {
+        if (arguments[i] == null) {
+          return;
+        }
+        return fn.apply(this, arguments);
+      }
+    }
+  };
+}
+
+// es6 rewrite
+const maybe = f => (...args) => {
+  const argsLen = args.length;
+  if (argsLen === 0) {
+    return;
+  }
+
+  for (let i = 0; i < argsLen; ++i) {
+    if (args[i] === null) {
+      return;
+    }
+  }
+
+  return fn(...args);
+}
