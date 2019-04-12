54b85b985f696f866b51d80a33f67c51e8db93af
pui <5474275+puiutucutu@users.noreply.github.com>
2019-04-12 19:59:29 -0400

    Create joinWithDelimiter.js

---
+++ src/strings/joinWithDelimiter.js
@@ -0,0 +1,9 @@
+/**
+ * Abstract generic text splitting function.
+ *
+ * @param {String|RegExp} delimiter
+ * @return {function(text: String[]): String}
+ */
+const joinWithDelimiter = delimiter => items => items.join(delimiter);
+
+export { joinWithDelimiter } 
