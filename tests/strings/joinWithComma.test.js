3020d5369f2632760795f314a3dfc4a2f02a7549
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-13 17:49:44 -0400

    Changed test to use end instead of plan

--- tests/strings/joinWithComma.test.js
+++ tests/strings/joinWithComma.test.js
@@ -2,9 +2,10 @@ import test from "tape";
 import { joinWithComma } from "../../src/strings";
 
 test("Returns expected value", function(assert) {
-  assert.plan(1);
   const actual = ["a", "b", "c"];
   const expected = "a,b,c";
 
   assert.equal(joinWithComma(actual), expected);
+
+  assert.end();
 });
