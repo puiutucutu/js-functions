eea7c9c4bbef3c6ba07b4e645e2503f6d871ae58
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-13 17:39:19 -0400

    Added index files

---
+++ src/strings/index.js
@@ -0,0 +1,27 @@
+import { joinWithComma } from "./joinWithComma";
+import { joinWithDelimiter } from "./joinWithDelimiter";
+import { joinWithNewLine } from "./joinWithNewLine";
+import { map } from "./map";
+import { removeCommas } from "./removeCommas";
+import { removeFalseyStrings } from "./removeFalseyStrings";
+import { splitByComma } from "./splitByComma";
+import { splitByDelimiter } from "./splitByDelimiter";
+import { splitByNewLines } from "./splitByNewLines";
+import { splitByWhiteSpace } from "./splitByWhiteSpace";
+import { trim } from "./trim";
+import { trims } from "./trims";
+
+export {
+  joinWithComma,
+  joinWithDelimiter,
+  joinWithNewLine,
+  map,
+  removeCommas,
+  removeFalseyStrings,
+  splitByComma,
+  splitByDelimiter,
+  splitByNewLines,
+  splitByWhiteSpace,
+  trim,
+  trims
+};
