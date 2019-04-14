c18032f95697a148050a6db6242970da33959827
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-14 19:33:42 -0400

    Ran test through prettier

--- tests/strings/joinWithNewLine.js
+++ tests/strings/joinWithNewLine.js
@@ -3,10 +3,9 @@ import { joinWithNewLine } from "../../src/strings";
 
 test("Returns expected value", function(assert) {
   const actual = ["a", "b", "c"];
-  const expected =  'a\r\nb\r\nc';
+  const expected = "a\r\nb\r\nc";
 
   assert.equal(joinWithNewLine(actual), expected);
 
   assert.end();
 });
-
