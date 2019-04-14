399a0be5bb3ffc9141d066060a34b8e4f48e45e6
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-14 19:53:53 -0400

    Added test for deeply nested arrays

--- tests/flatten.test.js
+++ tests/flatten.test.js
@@ -2,6 +2,15 @@ import test from "tape";
 import { flatten } from "../src";
 
 test("`flatten` returns expected value", function(assert) {
+  const actual = [[1, 2, 3], [4, 5, 6]];
+
+  const expected = [1, 2, 3, 4, 5, 6];
+
+  assert.deepEqual(flatten(actual), expected);
+  assert.end();
+});
+
+test("`flatten` can handle deeply nested arrays (i.e., four levels deep)", function(assert) {
   const actual = [
     [1, 2, 3],
     [[4, 5, 6], [7, 8, 9]],
