1e2a836246f504438d0347b27a3fb43f560f1127
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-16 09:55:26 -0400

    Updated jsdoc again

--- src/pipe.js
+++ src/pipe.js
@@ -2,7 +2,7 @@
  * Reduces N amount of functions over some value of `x`.
  *
  * @param {...Function} fns Each fn must have an arity of 1
- * @return {function(*): *} Supplied arg `x` reduced over each fn * of fns
+ * @return {function(*): *} Some value `x` reduced over each fn * of fns
  */
 const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
 
