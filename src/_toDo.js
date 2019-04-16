36f853883f9acd208deb5664acb31bf0ede08cc1
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-16 13:15:51 -0400

    Update _toDo.js

--- src/_toDo.js
+++ src/_toDo.js
@@ -1,11 +1,32 @@
-  areArraysEqualShallow(a, b) {
-    if (!a || !b) return false;
-    if (!Array.isArray(a) || !Array.isArray(b)) return false;
-    return a.length === b.length && a.every((v, i) => v === b[i]);
-  }
+/**
+ * Whether or not every element in `a` matches the value in the same
+ * position but in `b`.
+ *
+ * @param {Array} a
+ * @param {Array} b
+ * @return {Boolean}
+ */
+function areArraysEqualShallow(a, b) {
+  if (!a || !b) return false;
+  if (!Array.isArray(a) || !Array.isArray(b)) return false;
 
-  areArraysSameShallow(a, b) {
-    if (!a || !b) return false;
-    if (!Array.isArray(a) || !Array.isArray(b)) return false;
-    return a.length === b.length && a.every(el => b.includes(el));
-  }
+  return a.length === b.length && a.every((v, i) => v === b[i]);
+}
+
+export { areArraysEqualShallow };
+
+/**
+ * Whether or not every value of `a` is somewhere in `b`.
+ *
+ * @param {Array} a
+ * @param {Array} b
+ * @return {Boolean}
+ */
+function areArraysSameShallow(a, b) {
+  if (!a || !b) return false;
+  if (!Array.isArray(a) || !Array.isArray(b)) return false;
+
+  return a.length === b.length && a.every(el => b.includes(el));
+}
+
+export { areArraysSameShallow };
