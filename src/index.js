2f35617705ac9235a08c86e78f33b8de41f8c6d1
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-16 21:11:31 -0400

    Fixed export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import * as strings from "./strings";
 import { compose } from "./compose";
-import { composeTwo } from "./composeTwo";
+import { composes } from "./composes";
 import { flatten } from "./flatten";
 import { isEmptyObject } from "./isEmptyObject";
 import { maybe } from "./maybe";
 import { pipe } from "./pipe";
 
-export { strings, compose, composeTwo, flatten, isEmptyObject, maybe, pipe };
+export { strings, compose, composes, flatten, isEmptyObject, maybe, pipe };
