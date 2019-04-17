06ad5c9016adb3caf1364aff630a3f03aa77aa8a
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-16 21:14:48 -0400

    Simplified test message

--- tests/compose.test.js
+++ tests/compose.test.js
@@ -4,16 +4,22 @@ import { compose } from "../src";
 test("testing `compose` fn", function(t) {
   let fnCall, actual, expected;
 
-  fnCall = compose(() => {}, () => {});
+  fnCall = compose(
+    () => {},
+    () => {}
+  );
   actual = Object.prototype.toString.call(fnCall);
   expected = "[object Function]";
   t.equal(actual, expected, "`compose` returns a function on first call");
 
   const addsFive = a => a + 5;
   const multipliesBySix = a => a * 6;
-  actual = compose(addsFive, multipliesBySix)(2);
+  actual = compose(
+    addsFive,
+    multipliesBySix
+  )(2);
   expected = 17;
-  t.equal(actual, expected, "returns the expected value on final invocation");
+  t.equal(actual, expected, "returns the expected value");
 
   t.end();
 });
