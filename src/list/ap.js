a45ce15dfb1234233cfa4e01d815ccc8ba248592
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 13:58:25 -0400

    Update ap.js

--- src/list/ap.js
+++ src/list/ap.js
@@ -1,7 +1,9 @@
-const ap = (...fns) => arr => {
+const apES5 = (...fns) => arr => {
   return fns.reduce(function(acc, f) {
     return acc.map(f);
   }, arr);
 };
+
+const ap = (...fns) => x => fns.reduce((acc, f) => acc.map(f), x)
 
 export { ap };
