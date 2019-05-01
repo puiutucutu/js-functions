55c1eab567f1df064748ab1353bb2ddb489f2f20
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 19:34:40 -0400

    Renamed fn

--- src/list/flatMap.js
+++ src/list/flatMap.js
@@ -3,17 +3,17 @@ import { flatten } from "./flatten";
 import { compose } from "./compose";
 
 // different implementations
-let flatMapEx;
-flatMapEx = f => x => map(f)(x).reduce((xs, ys) => [...xs, ...ys]);
-flatMapEx = f => x => x.map(f).reduce((xs, ys) => [...xs, ...ys], []);
-flatMapEx = f => x => x.map(f).reduce((xs, ys) => [...xs, ...ys]);
-flatMapEx = f => x => Array.prototype.concat.apply([], x.map(f));
-flatMapEx = f => x => [].concat(...map(f)(x));
-flatMapEx = f => x => [].concat(...x.map(f));
+let flatMapAlt;
+flatMapAlt = f => x => map(f)(x).reduce((xs, ys) => [...xs, ...ys]);
+flatMapAlt = f => x => x.map(f).reduce((xs, ys) => [...xs, ...ys], []);
+flatMapAlt = f => x => x.map(f).reduce((xs, ys) => [...xs, ...ys]);
+flatMapAlt = f => x => Array.prototype.concat.apply([], x.map(f));
+flatMapAlt = f => x => [].concat(...map(f)(x));
+flatMapAlt = f => x => [].concat(...x.map(f));
 
 // implemented using own lib fns
-flatMapEx = f => x => flatten(map(f)(x));
-flatMapEx = f => x =>
+flatMapAlt = f => x => flatten(map(f)(x));
+flatMapAlt = f => x =>
   compose(
     flatten,
     map(f)
