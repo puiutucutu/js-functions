525ba80121e8421c6831ddc3459350678b9f6e7b
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 19:29:59 -0400

    Moved fn to own folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as strings from "./strings";
 import * as list from "./list";
-import { isEmptyObject } from "./isEmptyObject";
 import { maybe } from "./maybe";
 
-export { strings, list, isEmptyObject, maybe };
+export { strings, list, maybe };
