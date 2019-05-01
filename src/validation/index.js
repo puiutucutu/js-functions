d4d3a0c46f5b3af3f0c5f52839936029f36fa9cb
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 19:33:32 -0400

    Added exports

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,3 +1,15 @@
+import { getObjectType } from "./getObjectType";
+import { hasOwnProperty } from "./hasOwnProperty";
+import { isArray } from "./isArray";
 import { isEmptyObject } from "./isEmptyObject";
+import { isNumeric } from "./isNumeric";
+import { isString } from "./isString";
 
-export { isEmptyObject };
+export {
+  getObjectType,
+  hasOwnProperty,
+  isArray,
+  isEmptyObject,
+  isNumeric,
+  isString
+};
