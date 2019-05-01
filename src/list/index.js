569af7c2f872a8461f58577ba37d001cb7377181
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 19:28:42 -0400

    More imports

--- src/list/index.js
+++ src/list/index.js
@@ -1,10 +1,23 @@
 import { ap } from "./ap";
 import { compose } from "./compose";
 import { composes } from "./composes";
-import { flatten } from "./flatten";
 import { flatMap } from "./flatMap";
+import { flatten } from "./flatten";
+import { map } from "./map";
+import { mapNative } from "./mapNative";
 import { pipe } from "./pipe";
 import { reduce } from "./reduce";
 import { reduceNative } from "./reduceNative";
 
-export { ap, compose, composes, flatten, flatMap, pipe, reduce, reduceNative };
+export {
+  ap,
+  compose,
+  composes,
+  flatMap,
+  flatten,
+  map,
+  mapNative,
+  pipe,
+  reduce,
+  reduceNative
+};
