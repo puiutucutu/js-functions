e9d914921d32709b8955c0a3959856d7904f9f4c
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 12:47:12 -0400

    Fix mistake

--- tests/flatMap.test.js
+++ tests/flatMap.test.js
@@ -3,10 +3,10 @@ import { flatMap } from "../src";
 
 test("`flatMap` returns expected value", function(assert) {
   const nums = [1, 2, 3];
-  const origWithDouble = x => [x, x + 1];
+  const origWithDouble = x => [x, x * 2];
 
   const actual = flatMap(origWithDouble)(nums);
-  const expected = [1, 2, 2, 3, 3, 4];
+  const expected = [1, 2, 2, 4, 3, 6];
 
   assert.deepEqual(actual, expected);
   assert.end();
