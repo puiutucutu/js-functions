56ede88ff38101148d1688c44ace5aa7c37c27b5
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 17:55:08 -0400

    Merged into one test

--- tests/flatten.test.js
+++ tests/flatten.test.js
@@ -1,24 +1,18 @@
 import test from "tape";
 import { flatten } from "../src";
 
-test("`flatten` returns expected value", function(assert) {
-  const actual = [[1, 2, 3], [4, 5, 6]];
-  const expected = [1, 2, 3, 4, 5, 6];
+test("`flatten` function", function(assert) {
+  assert.deepEqual(
+    flatten([[1, 2, 3], [4, 5, 6]]),
+    [1, 2, 3, 4, 5, 6],
+    "returns expected value"
+  );
 
-  assert.deepEqual(flatten(actual), expected);
-  assert.end();
-});
+  assert.deepEqual(
+    flatten([[1, 2, 3], [[4, 5, 6], [7, 8, 9]], [10, 11, 12], [[[13, 14]]]]),
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+    "can handle deeply nested arrays (i.e., four levels deep)"
+  );
 
-test("`flatten` can handle deeply nested arrays (i.e., four levels deep)", function(assert) {
-  const actual = [
-    [1, 2, 3],
-    [[4, 5, 6], [7, 8, 9]],
-    [10, 11, 12],
-    [[[13, 14]]]
-  ];
-
-  const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
-  assert.deepEqual(flatten(actual), expected);
   assert.end();
 });
