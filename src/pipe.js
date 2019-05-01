f6ae35c51ee0a3be5c2008681e7a04b978f6cec1
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 11:22:38 -0400

    Improve comment

--- src/pipe.js
+++ src/pipe.js
@@ -1,5 +1,5 @@
 /**
- * Reduces N amount of functions over some value of `x`.
+ * Reduces N functions over some value of `x`.
  *
  * @param {...Function} fns Each fn must have an arity of 1
  * @return {function(*): *} Some value `x` reduced over each fn of fns
