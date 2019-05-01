584021e7bfc6b79842ec6efc993708ef17d5d2ee
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 12:31:03 -0400

    Create flatMap.js

---
+++ src/flatMap.js
@@ -0,0 +1,15 @@
+import { map } from "./map";
+
+// different implementations
+let flatMapEx;
+flatMapEx = f => x => map(f)(x).reduce((xs, ys) => [...xs, ...ys]);
+flatMapEx = f => x => x.map(f).reduce((xs, ys) => [...xs, ...ys], []);
+flatMapEx = f => x => x.map(f).reduce((xs, ys) => [...xs, ...ys]);
+flatMapEx = f => x => Array.prototype.concat.apply([], x.map(f));
+flatMapEx = f => x => [].concat(...map(f)(x));
+flatMapEx = f => x => [].concat(...x.map(f));
+
+// actual definition used
+const flatMap = f => x => [].concat(...map(f)(x));
+
+export { flatMap };
