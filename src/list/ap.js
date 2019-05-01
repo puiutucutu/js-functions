2949ea321e0359e9cfa16153a02c4842ff26adb6
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 15:28:48 -0400

    Update ap.js

--- src/list/ap.js
+++ src/list/ap.js
@@ -4,6 +4,16 @@ const apES5 = (...fns) => arr => {
   }, arr);
 };
 
+/*
+
+const ap = (...fns) => x => Array.prototype.reduce.call(
+  fns, 
+  (result, f) => result.map(f), 
+  x
+);
+
+*/
+
 // applies a list of functions to a list of values
 const ap = (...fns) => x => fns.reduce((acc, f) => acc.map(f), x);
 
