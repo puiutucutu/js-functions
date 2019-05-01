fa1b4a53adabcb5ff3c8cf1358d90d4d24acddfd
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 17:52:58 -0400

    Changed internal

--- src/list/ap.js
+++ src/list/ap.js
@@ -1,14 +1,12 @@
-/*
+/**
+ * Applies a list of functions to a list of values.
+ *
+ * @param fns
+ * @return {function(x: *[]): *[]}
+ */
+const ap = (...fns) => x =>
+  Array.prototype.reduce.call(fns, (acc, f) => acc.map(f), x);
 
-const ap = (...fns) => x => Array.prototype.reduce.call(
-  fns, 
-  (result, f) => result.map(f), 
-  x
-);
-
-*/
-
-// applies a list of functions to a list of values
-const ap = (...fns) => x => fns.reduce((acc, f) => acc.map(f), x);
+const apAlt = (...fns) => x => fns.reduce((acc, f) => acc.map(f), x);
 
 export { ap };
