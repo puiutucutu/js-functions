fd4acff6061ce74ba980144c727bde0c9bb75bf2
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 14:04:25 -0400

    Update ap.js

--- src/list/ap.js
+++ src/list/ap.js
@@ -5,7 +5,7 @@ const apES5 = (...fns) => arr => {
 };
 
 // applies a list of functions to a list of values
-// ap :: Apply f => f (a -> b -> ... n) -> f a -> f b -> f n
+// ap :: f => f (a -> b -> ... n) -> f a -> f b -> f n
 const ap = (...fns) => x => fns.reduce((acc, f) => acc.map(f), x);
 
 export { ap };
