8b7e02b365af5fca41b5daa3a3b74ca61e551c3e
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 21:35:50 -0400

    Comment fix

--- src/list/flatten.js
+++ src/list/flatten.js
@@ -1,5 +1,6 @@
 /**
- * Recursively flattens an array of items of varying arities into an array with an arity of one.
+ * Recursively flattens an array of items of varying arities into an array
+ * with an arity of one.
  *
  * @param {Array} items
  * @return {Array} The array of arrays reduced to a 1-d array
