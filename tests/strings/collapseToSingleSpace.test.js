ef61975e0bbd9175a9f173b04c9a800acd8088db
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-02 18:57:58 -0400

    Added test fn

---
+++ tests/strings/collapseToSingleSpace.test.js
@@ -0,0 +1,15 @@
+import test from "tape";
+import { collapseToSingleSpace } from "../../src/strings";
+
+test("`collapseToSingleSpace` fn", function(assert) {
+  const actual = "lorem  ipsum   dolor    sit     amet";
+  const expected = "lorem ipsum dolor sit amet";
+
+  assert.deepEqual(
+    collapseToSingleSpace(actual),
+    expected,
+    "returns expected value"
+  );
+
+  assert.end();
+});
