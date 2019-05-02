0b95392f3c02d3cabce41fcac7f7c2f4361e5747
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-02 13:06:02 -0400

    Update isEmptyString.js

--- src/validation/isEmptyString.js
+++ src/validation/isEmptyString.js
@@ -1,7 +1,9 @@
+import { isString } from "./isString";
+
 /**
  * @param {String} s
  * @return {Boolean}
  */
-export const isEmptyString = s => !!s
+export const isEmptyString = s => isString(s) && s !== "";
 
 export { isEmptyString }
