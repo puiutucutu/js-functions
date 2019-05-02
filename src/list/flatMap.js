17e44de9083eb2e7f700de1b88729c2b768877b0
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-01 22:40:40 -0400

    Added link and expanded fn

--- src/list/flatMap.js
+++ src/list/flatMap.js
@@ -3,11 +3,13 @@ import { flatten } from "./flatten";
 import { compose } from "./compose";
 
 // different implementations
+// {@see https://stackoverflow.com/questions/10865025/merge-flatten-an-array-of-arrays/25804569#25804569}
 let flatMapAlt;
 flatMapAlt = f => x => map(f)(x).reduce((xs, ys) => [...xs, ...ys]);
 flatMapAlt = f => x => x.map(f).reduce((xs, ys) => [...xs, ...ys], []);
 flatMapAlt = f => x => x.map(f).reduce((xs, ys) => [...xs, ...ys]);
 flatMapAlt = f => x => Array.prototype.concat.apply([], x.map(f));
+flatMapAlt = f => x => Array.prototype.concat.call([], ...x.map(f));
 flatMapAlt = f => x => [].concat(...map(f)(x));
 flatMapAlt = f => x => [].concat(...x.map(f));
 
@@ -23,6 +25,6 @@ flatMapAlt = f => x =>
  * @param {function(*): *} f
  * @return {function(*[]): *[]}
  */
-const flatMap = f => x => [].concat(...x.map(f));
+const flatMap = f => x => Array.prototype.concat(...x.map(f));
 
 export { flatMap };
