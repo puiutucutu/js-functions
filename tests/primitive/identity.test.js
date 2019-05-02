9aa317b3ea2204e53a80a6c0ceb163ac7cbc2cab
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-02 18:55:54 -0400

    Added test

---
+++ tests/primitive/identity.test.js
@@ -0,0 +1,15 @@
+import test from "tape";
+import { identity } from "../../src/primitive";
+
+test("`identity` fn", function(assert) {
+  assert.deepEqual(identity("a"), "a", "returns expected value");
+  assert.deepEqual(identity([1,2,3]), [1,2,3], "returns expected array value");
+  assert.deepEqual(identity(""), "", "returns empty string when empty string supplied");
+  assert.deepEqual(identity(false), false, 'returns expected value when supplied `false`');
+  assert.deepEqual(identity(0), 0, 'returns expected value when supplied `0`');
+  assert.deepEqual(identity(null), null, "returns expected value when supplied `null`");
+  assert.deepEqual(identity(undefined), undefined, "returns expected value when supplied `undefined`");
+  assert.deepEqual(identity(void 0), void 0, 'returns expected value when supplied `void 0`');
+
+  assert.end();
+});
