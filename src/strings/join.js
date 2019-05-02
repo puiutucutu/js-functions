7736f7b3dad025022f404e955990df5467928038
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-02 13:40:54 -0400

    Update join.js

--- src/strings/join.js
+++ src/strings/join.js
@@ -1,7 +1,7 @@
 /**
  * @param {String|RegExp} delimiter
- * @return {function(x: String|Number[]): String}
+ * @return {function(s: String[]|Number[]): String}
  */
-const join = delimiter => s => Array.prototype.join.call(s, delimiter);
+const join = delimiter => arr => Array.prototype.join.call(arr, delimiter);
 
 export { join }
