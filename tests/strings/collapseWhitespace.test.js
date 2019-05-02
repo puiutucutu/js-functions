a2621a7fc6052efaed47f30263bee598ff5a2288
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-02 18:58:31 -0400

    Added test

---
+++ tests/strings/collapseWhitespace.test.js
@@ -0,0 +1,15 @@
+import test from "tape";
+import { collapseWhitespace } from "../../src/strings";
+
+test("`collapseWhitespace` fn", function(assert) {
+  const actual = "lorem  ipsum   dolor    sit     amet";
+  const expected = "loremipsumdolorsitamet";
+
+  assert.deepEqual(
+    collapseWhitespace(actual),
+    expected,
+    "returns expected value"
+  );
+
+  assert.end();
+});
