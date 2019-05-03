d113de9e2608b9d92ea067ac26ebb2d81cc3ba33
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 19:23:01 -0400

    Updated test

--- tests/list/reduce.test.js
+++ tests/list/reduce.test.js
@@ -3,7 +3,7 @@ import { reduce } from "../../src/list";
 
 test("`reduce` function", function(t) {
   const reducer = (prev, next) => prev + next;
-  const actual = reduce(0) (reducer) ([1, 2, 3, 4]);
+  const actual = reduce (reducer) (0) ([1, 2, 3, 4]);
   const expected = 10;
 
   t.deepEqual(actual, expected, "returns expected value");
