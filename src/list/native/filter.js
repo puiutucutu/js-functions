f4332892bbf5576e758313620a518a446c328e25
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 19:42:21 -0400

    Updated jsdoc

--- src/list/native/filter.js
+++ src/list/native/filter.js
@@ -1,8 +1,7 @@
 /**
- * @param {Function} predicate
+ * @param {function(currentValue: *): Boolean} predicate
  * @return {function(n: T[]): T[]}
  */
 const filter = predicate => n => Array.prototype.filter.call(n, predicate);
 
 export { filter };
-
