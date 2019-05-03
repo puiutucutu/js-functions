29604277fc893edaf470a1f9d4188c67e7996f71
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 19:21:35 -0400

    Full js doc var name

--- src/list/reduce.js
+++ src/list/reduce.js
@@ -1,5 +1,5 @@
 /**
- * @param {function(acc: T, curr: U)} reducer
+ * @param {function(accumulator: T, currentValue: U)} reducer
  * @return {function(initialValue: T): function(items: U[]): (T|*)}
  */
 const reduce = reducer => initial => items => {
