2fbe52f416fc5e17bf2dcd49a9a4d5cc89fc9263
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 13:15:31 -0400

    Added test

---
+++ tests/helpers/convertArrayOfObjectsToObjectOfObjects .test.js
@@ -0,0 +1,20 @@
+import test from "tape";
+import { convertArrayOfObjectsToObjectOfObjects  } from "../src/helpers/convertArrayOfObjectsToObjectOfObjects ";
+
+test("testing `convertArrayOfObjectsToObjectOfObjects ` fn", function(t) {
+
+  const data = [
+    { id: 1, brand: "bmw", model: "7 Series" },
+    { id: 2, brand: "mercedes", model: "S Class"}
+  ];
+  
+  const actual = {
+    "bmw": { id: 1, brand: "bmw", model: "7 Series" },
+    "mercedes": { id: 2, brand: "mercedes", model: "S Class"}
+  }
+
+  const expected = convertArrayOfObjectsToObjectOfObjects(data, "brand");
+  
+  t.equal(actual, expected, "returns expected result");
+  t.end();
+});
