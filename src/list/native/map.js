c0db90d650a8ae5a52d9ffe1af95e369d37c7ae3
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 17:25:13 -0400

    Update map.js

--- src/list/native/map.js
+++ src/list/native/map.js
@@ -1,5 +1,5 @@
 /**
- * @param {function(a): b} f {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Syntax}
+ * @param {function(a: *): b: *} f {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Syntax}
  * @return {function(x: a[]): b[]}
  */
 const map = f => x => Array.prototype.map.call(x, f);
