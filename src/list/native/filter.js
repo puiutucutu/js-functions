23e898c42fc38622be826bdb071248d5908da1b4
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 19:47:46 -0400

    Updated fn

--- src/list/native/filter.js
+++ src/list/native/filter.js
@@ -2,6 +2,8 @@
  * @param {function(currentValue: *): Boolean} predicate
  * @return {function(items: T[]): T[]}
  */
-const filter = predicate => items => items.filter(predicate);
+const filter = predicate => items => {
+  return Array.prototype.filter.bind(items)(predicate);
+};
 
 export { filter };
