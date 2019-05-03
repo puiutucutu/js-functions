2d3732046a7261f1447699d45a3ef60634302833
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 15:56:31 -0400

    Update removeItem.test.js

--- tests/helpers/removeItem.test.js
+++ tests/helpers/removeItem.test.js
@@ -3,10 +3,20 @@ import { removeItem   } from "../src/helpers/removeItem ";
 
 test("testing `removeItem  ` fn", function(t) {
   const queuedIds = [1, 2, 3, 4, 5];
-  const actual = [1, 2, 4, 5];
-
-  const expected = removeItem (3) (queuedIds);
   
-  t.equal(actual, expected, "returns expected result");
+  t.equal(
+    [1, 2, 4, 5], 
+    removeItem (3) (queuedIds), 
+    "returns expected result when supplied Number[]"
+  );
+
+  cosnt randomStrings = ["ab", "cd", "ef"];
+  
+  t.equal(
+    ["ab", "cd"],
+    removeItem ("ef") (randomStrings), 
+    "returns expected result when supplied String[]"
+  );
+  
   t.end();
 });
