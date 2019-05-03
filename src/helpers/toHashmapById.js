e2c936205508ed5a85c63f481bdbe492fc55f427
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 15:22:57 -0400

    Create toHashmapById.js

---
+++ src/helpers/toHashmapById.js
@@ -0,0 +1,12 @@
+import { toHashmapByPropNameValue } from "./toHashmapByPropNameValue";
+
+/**
+ * Takes an array of objects and converts them to a hashmap using the value of
+ * the `id` object property as the k in <k,v>.
+ *
+ * @param {array} items
+ * @returns {object}
+ */
+const toHashmapById = items => toHashmapByPropNameValue(items, "id");
+
+export { toHashmapById }
