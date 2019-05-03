2dab6d36eec0be6f623e7bf955d97cdcdde925f8
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 19:19:30 -0400

    Updated fn

--- src/list/reduce.js
+++ src/list/reduce.js
@@ -5,13 +5,14 @@ import { map } from "./map";
  * @return {function(*): function(items: U[]): T}
  */
 const reduce = initial => reducer => items => {
-  let acc = initial;
+  let accumulator = initial;
+
   const len = items.length;
   for (let i = 0; i < len; i++) {
-    acc = reducer(acc, items[i], i, items);
+    accumulator = reducer(accumulator, items[i]);
   }
 
-  return acc;
+  return accumulator;
 };
 
 export { reduce };
