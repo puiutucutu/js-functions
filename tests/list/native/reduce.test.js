002a4cf4e798fbbf18e3fe075ea504e99826d6b4
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 19:10:50 -0400

    Updated test

--- tests/list/native/reduce.test.js
+++ tests/list/native/reduce.test.js
@@ -1,8 +1,9 @@
 import test from "tape";
 import { reduce } from "../../../src/list/native";
 
-test("`reduce` function", function(t) {
-  const actual = reduce(0)((prev, next) => prev + next)([1, 2, 3, 4]);
+test("`reduce` function using native implementation", function(t) {
+  const reducer = (prev, next) => prev + next;
+  const actual = reduce (0) (reducer) ([1, 2, 3, 4]);
   const expected = 10;
 
   t.deepEqual(actual, expected, "returns expected value");
