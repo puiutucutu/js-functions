be6f9e20b1405fd27e37c6346c1c6580afb782ca
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-02 20:34:06 -0400

    Updated code spacing

--- src/strings/collapseWhitespace.js
+++ src/strings/collapseWhitespace.js
@@ -6,6 +6,6 @@ import { replace } from "./replace";
  * @param {String} s
  * @return {String}
  */
-const collapseWhitespace = s => replace(/\s+/gi) ("") (s);
+const collapseWhitespace = s => replace (/\s+/gi) ("") (s);
 
 export { collapseWhitespace };
