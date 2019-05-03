978ed4491d71834815071faba57cae9cf51417e6
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 16:03:01 -0400

    Update immutablyRemoveHashmapItemByKey.js

--- src/helpers/immutablyRemoveHashmapItemByKey.js
+++ src/helpers/immutablyRemoveHashmapItemByKey.js
@@ -4,13 +4,11 @@
  *
  * Note that this function only works on an object nested one level deep.
  *
- * @param {object} hashmap
- * @param {string} key Coercing to string in argument list since object property
- * names can only be strings.
- * @return {object} Object with all of the original properties with the
- * exception of the property denoted by `key`.
+ * @param {String} key
+ * @return {function({}): {}} Object with all of the original properties
+ * except the property denoted by `key`.
  */
-const immutablyRemoveHashmapItemByKey = (hashmap, key = String(key)) =>
+const immutablyRemoveHashmapItemByKey = key => hashmap =>
   Object.keys(hashmap) // the k's in <K,V> for all hashmap items
     .filter(k => k !== key)
     .reduce((acc, k) => ({ ...acc, [k]: hashmap[k] }), {});
