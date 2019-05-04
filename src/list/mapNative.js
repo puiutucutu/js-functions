d9053536e41f80e6bf40db67a5aedc1499c3d8c7
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 20:03:20 -0400

    Remove file

--- src/list/mapNative.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {function(*): *} f {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Syntax}
- * @return {function(x: a[]): b[]}
- */
-const mapNative = f => x => Array.prototype.map.call(x, f);
-
-export { mapNative };
