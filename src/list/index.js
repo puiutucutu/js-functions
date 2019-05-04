3569ea87c8d7a5e7414281bcf23dfad5d2c81888
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-03 20:11:54 -0400

    Updated api

--- src/list/index.js
+++ src/list/index.js
@@ -1,13 +1,12 @@
 import * as native from "./native";
 export { native };
 
-export { map } from "./map";
-export { flatMap } from "./flatMap";
-export { reduce } from "./reduce";
-export { filter } from "./filter";
-export { composes } from "./composes";
 export { ap } from "./ap";
-export { mapNative } from "./mapNative";
-export { flatten } from "./flatten";
 export { compose } from "./compose";
+export { composes } from "./composes";
+export { filter } from "./filter";
+export { flatMap } from "./flatMap";
+export { flatten } from "./flatten";
+export { map } from "./map";
 export { pipe } from "./pipe";
+export { reduce } from "./reduce";
