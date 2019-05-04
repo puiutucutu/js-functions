dd88933646bc4bd0f1969f5b872be47e6942aa70
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 15:54:31 -0400

    Updated fn

--- src/list/native/reduce.js
+++ src/list/native/reduce.js
@@ -1,10 +1,16 @@
+import { uncurry } from "../uncurry";
+
 /**
  * reduce :: (a, b) => (a) => (b[]) -> a[]
  *
- * @param {function(accumulator: T, currentValue: U)} reducer
+ * @param {function(accumulator: T, currentValue: U)} f
  * @return {function(initialValue: T): function(items: U[]): (T|*)}
  */
-const reduce = reducer => initialValue => items =>
-  Array.prototype.reduce.call(items, reducer, initialValue);
+const reduce = f => y => xs =>
+  Array.prototype.reduce.call(
+    xs,
+    uncurry(f),
+    y
+  );
 
 export { reduce };
