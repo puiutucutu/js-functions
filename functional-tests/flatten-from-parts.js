431ec7c0489518e846b145f31aba88fa17bcdcad
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 23:26:44 -0400

    Updated example

--- functional-tests/flatten-from-parts.js
+++ functional-tests/flatten-from-parts.js
@@ -1,11 +1,32 @@
 import test from "tape";
-import { reduce } from "../src/list";
+import { reduce, apply } from "../src/list";
+import { map } from "../src/list/native";
+import { foldl } from "../src/list/native/foldl";
+import { isArray } from "../src/helpers/isArray";
+
+const concat = xs => ys => Array.prototype.concat(xs, ys);
+// const concat = xs => ys => (isArray(ys) ? [...xs, ...ys] : [...xs, ys]);
 
 test("flatten function implemented from smaller parts", function(t) {
-  const reducer = (prev) => (next) => prev + next;
-  const actual = reduce (reducer) (0) ([1, 2, 3, 4]);
-  const expected = 10;
+  const flatten = xs => foldl(concat)([])(xs);
+  const flattener = xs => flatten(
+    map (x => (isArray(x) ? flattener(x) : x)) (xs)
+  );
 
-  t.deepEqual(actual, expected, "returns expected value");
+  const xs = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+  const ys = [1, [2, [3, [4, [5], 6], 7], 8], 9];
+
+  t.deepEqual(
+    flatten(xs),
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    "returns expected value"
+  );
+
+  t.deepEqual(
+    flattener(ys),
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    "returns expected value"
+  );
+
   t.end();
 });
