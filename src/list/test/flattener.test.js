17cce181d8dd692b579fefc9d555dafdbff4e82d
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-05 00:25:22 -0400

    Remove dupe case

--- src/list/test/flattener.test.js
+++ src/list/test/flattener.test.js
@@ -3,12 +3,6 @@ import { flattener } from "../flattener";
 
 test("flatten", function(assert) {
   assert.deepEqual(
-    flattener([[1, 2, 3], [4, 5, 6]]),
-    [1, 2, 3, 4, 5, 6],
-    "returns expected value"
-  );
-
-  assert.deepEqual(
     flattener([1, [2, [3, [4, [5], 6], 7], 8], 9]),
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
     "can handle deeply nested arrays (i.e., four levels deep)"
