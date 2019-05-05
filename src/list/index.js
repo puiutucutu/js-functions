de4f59157c1be15429e45df7a6544591fc39d8c0
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 23:27:20 -0400

    Updated index

--- src/list/index.js
+++ src/list/index.js
@@ -3,12 +3,14 @@ export { native };
 
 export { apply } from "./apply";
 export { applyMany } from "./applyMany";
-export { concat } from "./concat";
 export { compose } from "./compose";
 export { composes } from "./composes";
+export { concat } from "./concat";
 export { filter } from "./filter";
 export { flatMap } from "./flatMap";
 export { flatten } from "./flatten";
+export { index } from "./index";
 export { map } from "./map";
 export { pipe } from "./pipe";
 export { reduce } from "./reduce";
+export { uncurry } from "./uncurry";
