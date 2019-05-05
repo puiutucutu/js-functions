8a2a1e47d34fbee799b03a21a5c15606392e9e4d
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 21:09:34 -0400

    Added case

--- tests/list/concat.test.js
+++ tests/list/concat.test.js
@@ -4,8 +4,14 @@ import { concat } from "../../src/list";
 test("concat", function(t) {
   t.deepEqual(
     concat ([1, 2]) ([3, 4]),
-    [1,2,3,4],
+    [1, 2, 3, 4],
     "returns expected value"
+  );
+
+  t.deepEqual(
+    concat ([1, 2]) ([3, [4, 5]]),
+    [1, 2, 3, [4, 5]],
+    "does not concat nested arrays more than one level deep"
   );
 
   t.end();
