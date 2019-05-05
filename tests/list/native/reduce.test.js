c26b6f294e4e6ff832ae1be69a10654265a06906
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 20:29:12 -0400

    Updated reduce test with new design

--- tests/list/native/reduce.test.js
+++ tests/list/native/reduce.test.js
@@ -2,7 +2,7 @@ import test from "tape";
 import { reduce } from "../../../src/list/native";
 
 test("`reduce` function using native implementation", function(t) {
-  const reducer = (prev, next) => prev + next;
+  const reducer = (prev) => (next) => prev + next;
   const actual = reduce (reducer) (0) ([1, 2, 3, 4]);
   const expected = 10;
 
