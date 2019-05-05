2043327a5352fdf8433ec56f47111084024b84d5
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 20:41:51 -0400

    Added apply

--- src/list/native/map.js
+++ src/list/native/map.js
@@ -1,8 +1,10 @@
+import { apply } from '../apply'
+
 /**
  * @param {function(a): b} f {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Syntax}
  * @return {function(x: a[]): b[]}
  */
-const map = f => x => Array.prototype.map.call(x, f);
+const map = f => x => Array.prototype.map.call(x, apply(f));
 
 export { map };
 
