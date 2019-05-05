4d0461328f96f12d4be7a3edf81ab11a6ccc988e
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 21:02:06 -0400

    Updated fn names

--- tests/list/pipe.test.js
+++ tests/list/pipe.test.js
@@ -3,19 +3,19 @@ import { pipe } from "../../src/list";
 
 test("`pipe` function", function(t) {
   const addOne = x => x + 1;
-  const multiplyTwo = x => x * 2;
+  const double = x => x * 2;
 
   t.deepEqual(
-    pipe (addOne, multiplyTwo) (1),
+    pipe (addOne, double) (1),
     4,
     "returns expected value when operation on non-array type"
   );
 
   const addsOne = xs => xs.map(x => x + 1);
-  const multipliesByTwo = xs => xs.map(x => x * 2);
+  const doubles = xs => xs.map(x => x * 2);
 
   t.deepEqual(
-    pipe (addsOne, multipliesByTwo) ([1, 2, 3]),
+    pipe (addsOne, doubles) ([1, 2, 3]),
     [4, 6, 8],
     "returns expected value when operating on array type"
   );
