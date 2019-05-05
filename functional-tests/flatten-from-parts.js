02e5b3a6fd3622e990efba3d12aae7a0b74821f6
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 23:50:58 -0400

    Updated test

--- functional-tests/flatten-from-parts.js
+++ functional-tests/flatten-from-parts.js
@@ -1,27 +1,28 @@
 import test from "tape";
-import { reduce, apply } from "../src/list";
+import { concat } from "../src/list";
 import { map } from "../src/list/native";
 import { foldl } from "../src/list/native/foldl";
 import { isArray } from "../src/helpers/isArray";
 
-const concat = xs => ys => Array.prototype.concat(xs, ys);
-// const concat = xs => ys => (isArray(ys) ? [...xs, ...ys] : [...xs, ys]);
+const flatten = xs => foldl (concat) ([]) (xs);
+const flattener = xs => flatten(
+  map (x => (isArray(x) ? flattener(x) : x)) (xs)
+);
 
-test("flatten function implemented from smaller parts", function(t) {
-  const flatten = xs => foldl (concat) ([]) (xs);
-  const flattener = xs => flatten(
-    map (x => (isArray(x) ? flattener(x) : x)) (xs)
-  );
+const xs = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const ys = [1, [2, [3, [4, [5], 6], 7], 8], 9];
 
-  const xs = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-  const ys = [1, [2, [3, [4, [5], 6], 7], 8], 9];
-
+test("flatten fn implemented from smaller parts", function(t) {
   t.deepEqual(
     flatten (xs),
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
     "returns expected value"
   );
 
+  t.end();
+});
+
+test("flattener fn", function(t) {
   t.deepEqual(
     flattener (ys),
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -29,4 +30,4 @@ test("flatten function implemented from smaller parts", function(t) {
   );
 
   t.end();
-});
+})
