90359c01063dc5547c3a4aaf142d897d5a35b645
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-05 00:30:49 -0400

    Updated fn

--- src/list/flatten.js
+++ src/list/flatten.js
@@ -1,9 +1,6 @@
 import { foldl } from "./native/foldl";
-import { concat } from './concat'
+import { concat } from "./concat";
 
-
-const flatten = (xs) => xs.reduce((a, b) => a.concat(b), [])
-// const flatten = xs => foldl (concat) ([]) (xs);
-
+const flatten = xs => foldl (concat) ([]) (xs);
 
 export { flatten };
