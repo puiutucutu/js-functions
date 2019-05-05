da2e7e707c0a80cf95daf15f472bd1a4f3b783f7
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-05 00:25:49 -0400

    Added fn

---
+++ src/list/flattener.js
@@ -0,0 +1,16 @@
+import { isArray } from "../helpers/isArray";
+
+/**
+ * Recursively flattens an array of items of varying arities into an array
+ * with an arity of one.
+ *
+ * @param {Array} items
+ * @return {Array} The array of arrays reduced to a 1-d array
+ */
+const flattener = items =>
+  items.reduce(
+    (acc, el) => (isArray(el) ? [...acc, ...flattener(el)] : [...acc, el]),
+    []
+  );
+
+export { flattener };
