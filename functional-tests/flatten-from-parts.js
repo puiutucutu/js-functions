90ae2bbc5bce04513b33a24cc5bf8e23d7101578
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 23:28:21 -0400

    Code style

--- functional-tests/flatten-from-parts.js
+++ functional-tests/flatten-from-parts.js
@@ -8,7 +8,7 @@ const concat = xs => ys => Array.prototype.concat(xs, ys);
 // const concat = xs => ys => (isArray(ys) ? [...xs, ...ys] : [...xs, ys]);
 
 test("flatten function implemented from smaller parts", function(t) {
-  const flatten = xs => foldl(concat)([])(xs);
+  const flatten = xs => foldl (concat) ([]) (xs);
   const flattener = xs => flatten(
     map (x => (isArray(x) ? flattener(x) : x)) (xs)
   );
@@ -17,13 +17,13 @@ test("flatten function implemented from smaller parts", function(t) {
   const ys = [1, [2, [3, [4, [5], 6], 7], 8], 9];
 
   t.deepEqual(
-    flatten(xs),
+    flatten (xs),
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
     "returns expected value"
   );
 
   t.deepEqual(
-    flattener(ys),
+    flattener (ys),
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
     "returns expected value"
   );
