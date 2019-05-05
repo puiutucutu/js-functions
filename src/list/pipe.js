4bc407a3a0165df1ac6cc8850f13a208643c6aa3
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-05 03:20:00 -0400

    Updated fn

--- src/list/pipe.js
+++ src/list/pipe.js
@@ -1,9 +1,15 @@
+import { reduce } from "./native";
+
 /**
  * Applies a list of functions to a single value.
  *
  * @param {...Function} fns Each fn must have an arity of 1
  * @return {function(U): T} Some value `x` reduced over each fn of fns
  */
-const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+const pipe = (...fns) => x => reduce
+  (acc => f => f(acc))
+  (x)
+  (fns)
+;
 
 export { pipe };
