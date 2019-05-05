6a134bdd638b1bb11c8a45e11acfdd1080050730
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 21:05:11 -0400

    Added jsdoc

--- src/list/applyMany.js
+++ src/list/applyMany.js
@@ -1,5 +1,11 @@
 import { pipe } from "./pipe";
 
+/**
+ * Applies a list of functions over a list.
+ *
+ * @param {...Function} fns
+ * @return {function(x: U[]): (T[]}
+ */
 const applyMany = (...fns) => xs => pipe (...fns) (xs);
 
 export { applyMany };
