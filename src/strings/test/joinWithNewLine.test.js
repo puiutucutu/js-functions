465c27ab4f10ab98769a4d27cc88be49e66596e2
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 21:20:56 -0400

    Updated test

--- src/strings/test/joinWithNewLine.test.js
+++ src/strings/test/joinWithNewLine.test.js
@@ -1,11 +1,12 @@
 import test from "tape";
 import { joinWithNewLine } from "../joinWithNewLine";
 
-test("Returns expected value", function(assert) {
-  const actual = ["a", "b", "c"];
-  const expected = "a\r\nb\r\nc";
-
-  assert.equal(joinWithNewLine(actual), expected);
+test("joinWithNewLine", function(assert) {
+  assert.deepEqual(
+    joinWithNewLine(["a", "b", "c"]),
+    "a\r\nb\r\nc",
+    "returns expected value"
+  );
 
   assert.end();
 });
