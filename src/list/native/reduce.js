eaee5be4693f6eaa64d9ab04ed9a7217852d80cb
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-04 20:31:21 -0400

    Change arg name

--- src/list/native/reduce.js
+++ src/list/native/reduce.js
@@ -4,7 +4,7 @@ import { uncurry } from "../uncurry";
  * reduce :: (a, b) => (a) => (b[]) -> a[]
  *
  * @param {function(accumulator: T, currentValue: U)} f
- * @return {function(initialValue: T): function(items: U[]): (T|*)}
+ * @return {function(initialValue: T): function(xs: U[]): (T|*)}
  */
 const reduce = f => accumulator => xs => Array.prototype.reduce.call
 (
