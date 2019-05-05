8f93bf528514f53318f3433cc228865310deb079
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-05 00:23:33 -0400

    Updated

--- src/list/flatten.js
+++ src/list/flatten.js
@@ -1,35 +1,6 @@
-/**
- * Recursively flattens an array of items of varying arities into an array
- * with an arity of one.
- *
- * @param {Array} items
- * @return {Array} The array of arrays reduced to a 1-d array
- */
-const flatten = items =>
-  items.reduce(
-    (acc, el) => (Array.isArray(el) ? [...acc, ...flatten(el)] : [...acc, el]),
-    []
-  );
+import { foldl } from "./native/foldl";
+import { concat } from './concat'
 
-/**
- * @param args
- * @return {*[]}
- */
-const concat = (...args) => Array.prototype.concat(...args);
-
-const flattenReduceConcat = (arr) => arr.reduce((a, b) => a.concat(b), []);
-const flattenConcatES6 = arrays => [].concat(...arrays);
-const flattenConcatES5 = arrays => [].concat.apply([], arrays);
-const flattenRecursiveES6 = arr =>
-  Array.isArray(arr)
-    ? [].concat(...arr.map(flattenRecursiveES6))
-    : [...arr];
-
-const flattenRecursiveAlternateES6 = arr =>
-  [].concat(
-    ...arr.map(el =>
-      Array.isArray(el) ? flattenRecursiveAlternateES6(el) : el
-    )
-  );
+const flatten = xs => foldl (concat) ([]) (xs);
 
 export { flatten };
