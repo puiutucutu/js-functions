0ba189b5450c7176a6dd52efb1d3c3eba0aa4489
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-06 23:57:08 -0400

    Added signature

--- src/list/pipe.js
+++ src/list/pipe.js
@@ -3,6 +3,8 @@ import { reduce } from "./native";
 /**
  * Applies a list of functions to a single value.
  *
+ * pipe :: ((a -> b), (b -> c), ... (y -> z)) -> a -> z
+ *
  * @param {...Function} fns Each fn must have an arity of 1
  * @return {function(U): T} Some value `x` reduced over each fn of fns
  * @signature pipe :: ([(a -> b), (b -> c), ..., (y -> z)]) -> a -> z
