94bd472ee7ee41e2655921b997e338861601807c
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-07 12:02:35 -0400

    Update applyMany.js

--- src/list/applyMany.js
+++ src/list/applyMany.js
@@ -1,7 +1,7 @@
 import { pipe } from "./pipe";
 
 /**
- * Applies a list of functions over a list.
+ * Applies a list of functions over a list. Each function must be unary and take a list as the parameter.
  *
  * applyMany :: ((a -> b), (b -> c), ...(y -> z)) -> [a] -> [z]
  *
