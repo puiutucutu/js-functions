ad2902bc8f205785857c8a24df986dec25957dff
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-07 00:48:31 -0400

    Added signature

--- src/list/native/filter.js
+++ src/list/native/filter.js
@@ -1,6 +1,7 @@
 /**
  * @param {function(T): boolean} predicate
  * @return {function(T[]): T[]}
+ * @signature (a -> Bool)) -> [a] -> [a]
  */
 const filter = predicate => xs => {
   return Array.prototype.filter.bind(xs)(predicate);
