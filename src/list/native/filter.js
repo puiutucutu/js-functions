d5be80ba50a3edb17d1e0a4002e7695a52c86e76
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-07 00:49:32 -0400

    Extra brace

--- src/list/native/filter.js
+++ src/list/native/filter.js
@@ -1,6 +1,6 @@
 /**
  * @param {function(T): boolean} predicate
- * @signature (a -> Bool)) -> [a] -> [a]
+ * @signature (a -> Bool) -> [a] -> [a]
  * @return {function(T[]): T[]}
  */
 const filter = predicate => xs => Array.prototype.filter.bind (xs) (predicate);
