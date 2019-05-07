ee63993b648d50f608df60c1f95a398d3587df98
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-07 00:39:13 -0400

    Updated fn file

--- src/list/pipe.js
+++ src/list/pipe.js
@@ -1,21 +1,23 @@
 import { reduce } from "./native";
 
+const pipeCurriedReduce = (...fns) => x =>
+  reduce
+  (acc => f => f(acc))
+  (x)
+  (fns)
+;
+
 /**
  * Applies a list of functions to a single value.
  *
  * Same implementation as using `Array.prototype.reduce`:
  *
- *    const pipe = (...fns) => x => fns.reduce((acc, f) => f(acc), x)
+ *    const pipe = Array.prototype.reduce.call(fns, (acc, f) => f(acc), x);
  *
  * @param {...Function} fns Each fn must have an arity of 1
  * @return {function(U): T} Some value `x` reduced over each fn of fns
  * @signature pipe :: ([(a -> b), (b -> c), ...(y -> z)]) -> a -> z
  */
-const pipe = (...fns) => x =>
-  reduce
-    (acc => f => f(acc))
-    (x)
-    (fns)
-;
+const pipe = (...fns) => x => fns.reduce((acc, f) => f(acc), x);
 
 export { pipe };
