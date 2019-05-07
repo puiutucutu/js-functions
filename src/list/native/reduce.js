8530c144171dad115c9d4414c30e33f727bf3fab
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-07 00:41:02 -0400

    Updated name to be more rescriptive

--- src/list/native/reduce.js
+++ src/list/native/reduce.js
@@ -6,11 +6,11 @@ import { uncurry } from "../uncurry";
  * @param {function(accumulator: T, currentValue: U)} reducer
  * @return {function(initialValue: T): function(xs: U[]): (T|*)}
  */
-const reduce = reducer => accumulator => xs => Array.prototype.reduce.call
+const reduce = reducer => accumulatorInitialValue => xs => Array.prototype.reduce.call
 (
   xs,
   uncurry(reducer),
-  accumulator
+  accumulatorInitialValue
 );
 
 export { reduce };
