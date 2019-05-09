ae5318048c7d3960d4e6875a4794810b76d43d11
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-08 20:37:43 -0400

    Streamlined fn

--- src/helpers/removeKeyFromObject.js
+++ src/helpers/removeKeyFromObject.js
@@ -1,4 +1,8 @@
 import { createMap } from "./createMap";
+import { compose } from '../list'
+import { reduce } from '../list/native'
+import { filter } from '../list/native'
+import { keysOf } from './keysOf'
 
 const Map = createMap();
 
@@ -12,40 +16,11 @@ const Map = createMap();
  * @return {function({}): {}} Object with all of the original properties
  * except the property denoted by `key`.
  */
-const removeKeyFromObjectNative = key => obj =>
-  Object.keys(obj)
-    .filter(k => k !== key)
-    .reduce((map, k) => ({ ...map, [k]: obj[k] }), Map);
-
-const filter = predicate => n => Array.prototype.filter.call(n, predicate);
-const reduce = reducer => initial => items => items.reduce(reducer, initial);
-const keysOf = obj => Object.keys(obj);
-const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
-const compose = f => g => x => f(g(x));
-
-const removeKeyFromObject = key => obj => {
-  const keys = keysOf(obj);
-  const keysToKeep = filter (k => k !== key) (keys);
-  return reduce((map, k) => ({ ...map, [k]: obj[k] })) (Map) (keysToKeep);
-};
-
-const removeKeyFromObjectUsingPipe = key => obj =>
-  pipe(
-    filter (k => k !== key),
-    reduce ((map, k) => ({ ...map, [k]: obj[k] })) (Map)
-  )(keysOf(obj));
-
-const removeKeyFromObjectUsingComposition = key => obj =>
-  compose (reduce((map, k) => ({ ...map, [k]: obj[k] })) (Map))
-          (filter(k => k !== key))
-          (keysOf(obj));
-
-const removeKeyFromObjectUsingCompositionClearer = key => obj => {
-  const keys = keysOf(obj);
-  const filterer = filter (k => k !== key);
-  const reducer = reduce ((map, k) => ({ ...map, [k]: obj[k] })) (Map);
-  
-  return compose (reducer) (filterer) (keys)
-}
+const removeKeyFromObject = key => obj =>
+  compose
+    (reduce(map => k => ({ ...map, [k]: obj[k] })) (Map))
+    (filter(k => k !== key))
+    (keysOf(obj))
+;
 
 export { removeKeyFromObject };
