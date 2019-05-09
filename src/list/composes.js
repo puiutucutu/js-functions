004b545543a24f08481a1f0fc4307ed433ff7232
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-08 20:39:51 -0400

    Updated fn

--- src/list/composes.js
+++ src/list/composes.js
@@ -1,18 +1,16 @@
 import { reduceRight } from './native'
 
-const composeWithCurriedReduceRight = (...fns) => x =>
-  reduceRight
-  ((acc) => f => f(acc))
-  (x)
-  (fns)
-;
-
 /**
  * composes :: ((z -> y), (x -> y), ... (b -> a)) -> z -> a
  *
  * @param {...Function} fns Each fn must have an arity of 1
  * @return {Function}
  */
-const composes = (...fns) => x => fns.reduceRight((acc, f) => f(acc), x);
+const composes = (...fns) => x =>
+  reduceRight
+  ((acc) => f => f(acc))
+  (x)
+  (fns)
+;
 
 export { composes };
