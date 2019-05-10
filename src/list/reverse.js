410482d1fc65af9c3d68baa2a018483179e5077a
admin <>
2019-05-10 14:32:48 -0400

    Added jsdoc

--- src/list/reverse.js
+++ src/list/reverse.js
@@ -1,5 +1,11 @@
 import { clone } from "./clone";
 
+/**
+ * reverse :: [a] -> [a]
+ *
+ * @param {*[]} xs
+ * @return {*[]}
+ */
 const reverse = xs => Array.prototype.reverse.call(clone(xs));
 
 export { reverse };
