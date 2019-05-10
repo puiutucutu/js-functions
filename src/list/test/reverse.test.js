44237e8e6ebefba888c763e318a9953d0b54c089
admin <>
2019-05-10 14:31:57 -0400

    Added test case

--- src/list/test/reverse.test.js
+++ src/list/test/reverse.test.js
@@ -5,6 +5,17 @@ test("reverse", function(t) {
   const actual = reverse ([1,2,3]);
   const expected = [3,2,1];
 
-  t.deepEqual(actual, expected, "returns expected value");
+  t.deepEqual(
+    actual,
+    expected,
+    "returns expected value"
+  );
+
+  t.deepEqual(
+    reverse ([]),
+    [],
+    "returns expected value when empty array"
+  );
+
   t.end();
 });
