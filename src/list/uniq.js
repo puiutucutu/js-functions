f4246b9bfc7c6f15b2f38cbce2a83ac9471a2167
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-10 12:25:26 -0400

    Bugfix

--- src/list/uniq.js
+++ src/list/uniq.js
@@ -1,6 +1,6 @@
 function uniq(xs) {
-  return [].reduce(function(acc, x) {
-    return acc.includes(next) ? acc : [...acc, next];
+  return xs.reduce(function(acc, x) {
+    return acc.includes(x) ? acc : [...acc, x];
   }, [])
 }
 
