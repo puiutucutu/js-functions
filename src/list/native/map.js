9e3011cd118b25485e9a867a8905d3cd1d1a8700
admin <>
2019-05-10 14:13:24 -0400

    Updated signature

--- src/list/native/map.js
+++ src/list/native/map.js
@@ -3,12 +3,12 @@ import { uncurryThree } from "../uncurryThree";
 /**
  * map :: (a -> b) -> [a] -> [b]
  *
- * @param {function(a): b} f {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Syntax}
+ * @param {function(a, index: Number, array: *[]): b} f {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Syntax}
  * @return {function(xs: a[]): b[]}
  */
 const map = f => xs => Array.prototype.map.call(
   xs,
-  uncurryThree(f) //=> (currentValue, index, array) => f (currentValue) (index) (array)
+  uncurryThree(f) //=> (currentValue, index, originalArray) => f (currentValue) (index) (array)
 );
 
 export { map };
