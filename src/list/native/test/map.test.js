d3563b0150b89bc7550672e9cf22a7517e7f8f71
admin <>
2019-05-10 14:13:46 -0400

    Code clean

--- src/list/native/test/map.test.js
+++ src/list/native/test/map.test.js
@@ -2,7 +2,7 @@ import test from "tape";
 import { map } from "../map";
 
 test("map (native implementation)", function(t) {
-  const doubler = x => i => xs => x * 2
+  const doubler = x => i => xs => x * 2;
 
   const nums = [1, 2, 3];
   const actual = map (doubler) (nums);
