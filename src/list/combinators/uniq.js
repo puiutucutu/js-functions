7d865b6f5925dee9d3f83709cc47651ce842a94a
admin <>
2019-05-10 14:00:08 -0400

    Added signature

--- src/list/combinators/uniq.js
+++ src/list/combinators/uniq.js
@@ -1,6 +1,12 @@
 import { reduce } from "../native/reduce";
 import { includes } from "../includes";
 
+/**
+ * uniq :: [a] -> [a]
+ *
+ * @param {T[]} xs
+ * @return {T[]}
+ */
 const uniq = xs =>
   reduce
     (uniques => x => (includes (x) (uniques) ? uniques : [...uniques, x]))
