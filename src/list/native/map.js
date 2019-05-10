063d36ae27a60cca57b8d0f93a13ad1105caa8ab
admin <>
2019-05-10 14:11:24 -0400

    Added example

--- src/list/native/map.js
+++ src/list/native/map.js
@@ -1,13 +1,4 @@
-import { apply } from "../apply";
 import { uncurryThree } from "../uncurryThree";
-
-const mapNondeclarative = f => xs => Array.prototype.map.call(xs, apply(f));
-
-const mapUncurriedManually = f => xs => Array.prototype.map.call
-(
-  xs,
-  (currentValue, index, array) => f (currentValue) (index) (array)
-);
 
 /**
  * map :: (a -> b) -> [a] -> [b]
@@ -17,20 +8,7 @@ const mapUncurriedManually = f => xs => Array.prototype.map.call
  */
 const map = f => xs => Array.prototype.map.call(
   xs,
-  uncurryThree(f)
+  uncurryThree(f) //=> (currentValue, index, array) => f (currentValue) (index) (array)
 );
-
-/* example */
-
-const doubled = nums =>
-  map
-    (currentValue => currIndex => original => currentValue * 2)
-    (nums)
-;
-
-const doubledConcise = nums => map (x => i => xs => x * 2) (nums);
-
-const nums = [1,2,3];
-const doubledNums = doubled(nums); //=> [2,4,6]
 
 export { map };
---
+++ src/list/native/test/map.test.js
@@ -0,0 +1,13 @@
+import test from "tape";
+import { map } from "../map";
+
+test("map (native implementation)", function(t) {
+  const doubler = x => i => xs => x * 2
+
+  const nums = [1, 2, 3];
+  const actual = map (doubler) (nums);
+  const expected = [2, 4, 6];
+
+  t.deepEqual(actual, expected, "returns expected value");
+  t.end();
+});
