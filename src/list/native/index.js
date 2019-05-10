5ecc44686cf64a1fc0cb549bd04de49893e3f945
admin <>
2019-05-10 15:33:15 -0400

    Updated export

--- src/list/native/index.js
+++ src/list/native/index.js
@@ -1,5 +1,6 @@
+export { filter } from "./filter";
 export { foldl } from "./foldl";
 export { map } from "./map";
-export { filter } from "./filter";
+export { mapWithNativeCallback } from "./mapWithNativeCallback";
 export { reduce } from "./reduce";
 export { reduceRight } from "./reduceRight";
