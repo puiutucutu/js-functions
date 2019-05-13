dbf4b5d094f46236a2cfcd5b0c8865f46fc6470f
admin <>
2019-05-13 16:23:07 -0400

    Code style

--- src/list/mapRecursively.js
+++ src/list/mapRecursively.js
@@ -1,11 +1,12 @@
-import { lengthRecursive as length } from './lengthRecursive'
-import { head } from './head'
-import { tail } from './tail'
+import { lengthRecursive as length } from "./lengthRecursive";
+import { head } from "./head";
+import { tail } from "./tail";
 
 // map :: (a->b) -> [a] -> [b]
-const map = f => xs => length(xs) === 0
-  ? []
-  : [f(head(xs)), ...map(f)(tail(xs))]
-;
+const map = f => xs =>
+  length(xs) === 0
+    ? []
+    : [f(head(xs)), ...map(f)(tail(xs))]
+  ;
 
 export { map as mapRecursively };
