bfc7aa4e01f1aa7e8fe56e97b791eca63528a764
admin <>
2019-05-13 16:20:46 -0400

    Updated fn

--- src/list/lengthRecursive.js
+++ src/list/lengthRecursive.js
@@ -1,5 +1,5 @@
 import { isArray } from "../helpers/isArray";
-import { tail } from './tail'
+import { tail } from "./tail";
 
 const toString = x => x.toString();
 
@@ -7,44 +7,15 @@ const toString = x => x.toString();
  * @param {*[]} xs
  * @return {Number}
  */
-function lengthRecursive(xs) {
-  if (!isArray (xs)) {
-    throw new TypeError("Argument `xs` must be of type array")
+const lengthRecursive = xs => {
+  if (!isArray(xs)) {
+    throw new TypeError("Argument `xs` must be of type array");
   }
 
-  const [, ...tail] = xs;
-  return toString(xs) === "" ? 0 : lengthRecursive (tail) + 1;
-}
+  return toString(xs) === ""
+    ? 0
+    : 1 + lengthRecursive(tail (xs))
+  ;
+};
 
 export { lengthRecursive };
-
-// // map :: (a->b) -> [a] -> [b]
-// const map = f => xs => {
-//   const [h,...t] = xs;
-//   return (xs).length === 0 ? [] : [f(h), ...map(f)(t)]
-// }
-//
-// const addsOne = x => x + 1;
-// const nums = [1,2,3];
-//
-// console.log(
-//   map(addsOne)(nums)
-// )
-//
-//
-//
-// const isArray = x => Array.isArray(x);
-// const toString = x => x.toString();
-//
-// /**
-//  * @param head
-//  * @param tail
-//  * @return {number}
-//  */
-// const lengthRecursive = ([head, ...tail]) =>
-//   isArray(tail) && toString(tail) === "" ? 1 : 1 + lengthRecursive(tail)
-//
-//
-// console.log(
-//   lengthRecursive([1,2])
-// )
