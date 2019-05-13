a5011e4707e061ea32010337c0e5c0924870a05b
admin <>
2019-05-13 16:26:15 -0400

    Code style

--- src/list/sum.js
+++ src/list/sum.js
@@ -1,5 +1,5 @@
 import { length } from "./length";
 
-const sum = ([h, ...t]) => (length([h, ...t]) === 0 ? 0 : h + sum(t));
+const sum = ([h, ...t]) => (length ([h, ...t]) === 0 ? 0 : h + sum (t));
 
 export { sum };
