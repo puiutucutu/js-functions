df0eb3520db8cc6d60eefe46700b431652d851f4
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-20 16:14:37 -0400

    Remove log

--- src/list/combinators/test/removeFalsey.test.js
+++ src/list/combinators/test/removeFalsey.test.js
@@ -4,7 +4,6 @@ import { removeFalsey } from "../removeFalsey";
 test("removeFalsey", function(t) {
   const values = [0, 0.0, "0", "", null, false, "false", []];
 
-  console.log(removeFalsey(values));
   t.deepEqual(
     ["0", "false", []],
     removeFalsey (values),
