208d92647a4887fcf51a758b5187a6b72d18750b
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-20 14:55:46 -0400

    Code style

--- src/object/omit.js
+++ src/object/omit.js
@@ -7,15 +7,14 @@ import { keys } from '../helpers'
  * @param {Array} ks
  * @return {function(o: Object): (Object)}
  */
-const omit = ks => o => {
-  return reduce
-    (acc => k => includes (k) (ks)
-      ? { ...acc }
-      : { ...acc, [k]: o[k] }
-    )
-    ({})
-    (keys (o))
-  ;
-}
+const omit = ks => o =>
+  reduce
+  (
+    acc => k => includes (k) (ks)
+    ? { ...acc }
+    : { ...acc, [k]: o[k] }
+  )
+  ({})
+  (keys(o))
 
 export { omit }
