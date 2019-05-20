d6a490a6a5ebc6fa4fd6d8195cece1737d1cf9d2
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-20 16:41:05 -0400

    Updated jsdoc

--- src/object/toHashmapById.js
+++ src/object/toHashmapById.js
@@ -2,7 +2,7 @@ import { toHashmapByPropNameValue } from "./toHashmapByPropNameValue";
 
 /**
  * Takes an array of objects and converts them to a hashmap using the value of
- * the `id` object property as the k in <k,v>.
+ * the `id` object property as the key in <k,v>.
  *
  * @param {Object[]} items
  * @returns {{ k: *, v: * }}
