29bae303594ce2530050a0e745df349b45d3d466
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-20 16:23:43 -0400

    Moved files around

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,3 @@
 export { convertArrayOfObjectsToMap } from "./convertArrayOfObjectsToMap";
 export { createMap } from "./createMap";
 export { removeItem } from "./removeItem";
-export { removeKeyFromObject } from "./removeKeyFromObject";
-export { toHashmapById } from "./toHashmapById";
-export { toHashmapByPropNameValue } from "./toHashmapByPropNameValue";
--- src/object/index.js
+++ src/object/index.js
@@ -2,3 +2,6 @@ export { createMap } from "./createMap";
 export { keys } from "./keys";
 export { omit } from "./omit";
 export { pick } from "./pick";
+export { removeKeyFromObject } from "./removeKeyFromObject";
+export { toHashmapById } from "./toHashmapById";
+export { toHashmapByPropNameValue } from "./toHashmapByPropNameValue";
