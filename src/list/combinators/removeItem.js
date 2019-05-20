7b3859291706c5b58b28c35bd2a9ed9fe648050c
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-20 17:37:36 -0400

    Remove wrong text

--- src/list/combinators/removeItem.js
+++ src/list/combinators/removeItem.js
@@ -3,8 +3,6 @@ import { filter } from "../filter";
 /**
  * Immutably removes an element from an array.
  *
- * filter :: a -> [a] -> [a]
- *
  * @param {String|Number} y
  * @return {function(*[]): []}
  */
