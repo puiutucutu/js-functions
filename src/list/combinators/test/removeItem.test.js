166c9f7beeda860c2b85a2f7e1031e005606ea90
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-20 17:35:37 -0400

    Updated test

--- src/list/combinators/test/removeItem.test.js
+++ src/list/combinators/test/removeItem.test.js
@@ -1,24 +1,21 @@
-// @todo fix this
-//
-// import test from "tape";
-// import { removeItem } from "../../src/helpers ";
-//
-// test("testing `removeItem  ` fn", function(t) {
-//   const queuedIds = [1, 2, 3, 4, 5];
-//
-//   t.equal(
-//     [1, 2, 4, 5],
-//     removeItem(3)(queuedIds),
-//     "returns expected result when supplied Number[]"
-//   );
-//
-//   const strs = ["ab", "cd", "ef"];
-//
-//   t.equal(
-//     ["ab", "cd"],
-//     removeItem("ef")(strs),
-//     "returns expected result when supplied String[]"
-//   );
-//
-//   t.end();
-// });
+import test from "tape";
+import { removeItem } from "../removeItem";
+
+test("removeItem", function(t) {
+  const nums = [1, 2, 3, 4, 5];
+  const strs = ["ab", "cd", "ef"];
+
+  t.deepEqual(
+    [1, 2, 4, 5],
+    removeItem (3) (nums),
+    "returns expected result when supplied Number[]"
+  );
+
+  t.deepEqual(
+    ["ab", "cd"],
+    removeItem ("ef") (strs),
+    "returns expected result when supplied String[]"
+  );
+
+  t.end();
+});
