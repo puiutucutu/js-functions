3ba2475a524244e2e907af52e25b652963db3a51
admin <>
2019-05-30 15:25:14 -0400

    Fix failing test

--- src/list/test/splitAt.test.js
+++ src/list/test/splitAt.test.js
@@ -2,7 +2,7 @@ import test from "tape";
 import { splitAt } from "../splitAt";
 
 test("splitAt", function(t) {
-  t.deepEqual(splitAt (1) ("abc"), "a", "returns expected value");
+  t.deepEqual(splitAt (1) ("abc"), ["a","bc"], "returns expected value");
   t.deepEqual(splitAt (2) ([1,2,3,4]), [[1,2],[3,4]], "returns expected value");
   t.end();
 });
