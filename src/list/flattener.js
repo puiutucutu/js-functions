2bee03639161e674dba48a75f74bfd97c3b8bca5
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-29 20:18:21 -0400

    Implemented flattener fn using reduce

--- src/list/flattener.js
+++ src/list/flattener.js
@@ -1,16 +1,18 @@
 import { isArray } from "../validation/isArray";
+import { reduce } from './reduce'
 
 /**
  * Recursively flattens an array of items of varying arities into an array
  * with an arity of one.
  *
- * @param {Array} items
+ * @param {Array} xs
  * @return {Array} The array of arrays reduced to a 1-d array
  */
-const flattener = items =>
-  items.reduce(
-    (acc, el) => (isArray(el) ? [...acc, ...flattener(el)] : [...acc, el]),
-    []
-  );
+const flattener = xs =>
+  reduce
+    ((acc) => (el) => (isArray(el) ? [...acc, ...flattener(el)] : [...acc, el]))
+    ([])
+    (xs)
+;
 
 export { flattener };
