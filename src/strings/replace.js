eeba7a8427f9f18187a702bbe6c540d741daab9e
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-31 00:10:18 -0400

    Fix fn typing

--- src/strings/replace.js
+++ src/strings/replace.js
@@ -1,5 +1,5 @@
 /**
- * replace :: (a) => (a) => (a) -> a
+ * replace :: (a) -> (a) -> (a) -> a
  *
  * @param {String|RegExp} searchValue
  * @return {function(a: string): function(a: string): string}
