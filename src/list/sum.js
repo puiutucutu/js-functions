347681248b4a6241809e11c1f5fa31de1d62949e
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-30 21:50:23 -0400

    Added jsdoc

--- src/list/sum.js
+++ src/list/sum.js
@@ -2,6 +2,10 @@ import { lengthRecursive as length } from "./lengthRecursive";
 import { head } from "./head";
 import { tail } from "./tail";
 
+/**
+ * @param {Number[]} xs
+ * @return {number}
+ */
 const sum = xs => (length (xs) === 0 ? 0 : head (xs) + sum (tail (xs)));
 
 export { sum };
