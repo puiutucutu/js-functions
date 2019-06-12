5fa6d7767a6f3cac7b6c8bdbca7bab989745e522
admin <>
2019-06-12 15:01:15 -0400

    Added docs

--- src/strings/truncate.js
+++ src/strings/truncate.js
@@ -2,6 +2,12 @@ import { splitByWhiteSpace } from "./splitByWhiteSpace";
 import { join } from "./join";
 import { head, pipe, splitAt } from "../list";
 
+/**
+ * splitAt :: Number -> String -> String
+ *
+ * @param {Number} numberOfWords
+ * @return {function(s: String): String}
+ */
 const truncate = numberOfWords => s =>
   pipe
   (
